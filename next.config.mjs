ef1606d4583b3ed23eb74d13c22dbfccb870b37f
imfj007 <imfj007@gmail@gmail.com>
2026-03-17 05:37:32 +0500

    fix: add static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 export default nextConfig;
