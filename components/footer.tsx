b1dfdf2fa90e4323d46941df010a40ac32f331d4
imfj007 <imfj007@gmail@gmail.com>
2026-03-17 05:56:19 +0500

    fix: footer all errors fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -49,7 +49,7 @@ export const Footer = () => {
         <p className="text-sm font-light md:text-base md:font-normal">
           Copyright &copy; {new Date().getFullYear()}{" "}
           <Link
-            href="https://sanidhy.me"
+            href="https://www.linkedin.com/in/faizan007/"
             target="_blank"
             rel="noreferrer noopener"
             className="text-purple"
@@ -74,7 +74,7 @@ export const Footer = () => {
             >
               <Image
                 src={profile.img}
-                alt={`profile-${profile.name}`}
+                alt="social-profile"
                 width={20}
                 height={20}
               />
