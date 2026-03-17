148e2b1e291f37d443b2f5c8d3d6be5490e9c35a
imfj007 <imfj007@gmail@gmail.com>
2026-03-17 05:25:00 +0500

    fix: tailwind svg import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import svgToDataUri from "mini-svg-data-uri";
+﻿const svgToDataUri = require("mini-svg-data-uri");
 import type { Config } from "tailwindcss";
 import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
